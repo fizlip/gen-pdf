9ea63e22082b8bee71cf9b5c0bc3dcffabfce440
fizlip <fillatino@gmail.com>
2024-02-26 16:29:11 +0100

    deserialize and render pdf based on json template

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use genpdf::Element;
 use genpdf::Alignment;
 use std::fs;
 use serde::Deserialize;
+use genpdf::Document;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
@@ -43,44 +44,63 @@ struct Row {
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-struct Document {
+struct SpekterDocument{
     Rows: Vec<Vec<Block>>
 }
 
-fn read_json() -> Document {
+fn read_json() -> SpekterDocument {
     let json_file = fs::read_to_string("./test.json").expect("Could not read file");
 
-    let json: Document = serde_json::from_str(&json_file)
+    let json: SpekterDocument = serde_json::from_str(&json_file)
         .expect("JSON not well formatted");
 
     json
 
 }
 
-fn parse_blocks(json:Document) {
+fn parse_blocks(mut doc: Document, json:SpekterDocument) -> Document {
+
     for r in json.Rows.iter() {
+        let column_weights: Vec<usize> = r.iter().map(|_| 1).collect();
+        let mut table = elements::TableLayout::new(column_weights);
+        table.set_cell_decorator(elements::FrameCellDecorator::new(false, false, false));
+        let mut row = table.row();
+
         for c in r.iter() {
-            println!("{}", c.content);
+
+            row.push_element(
+                elements::Paragraph::default()
+                    .string(c.content.to_string())
+                    .styled(style::Style::new().with_font_size(8)),
+            );
+
         }
+
+        row.push().expect("Invalid table row");
+        doc.push(table);
     }
+
+    doc
 }
 
 fn main() {
-
-    let doc:Document = read_json();
-    parse_blocks(doc);
-
     // Create
-    //let font_family = genpdf::fonts::from_files("./fonts", "Roboto", None)
-    //        .expect("Failed to load font family");
-    //let mut doc = genpdf::Document::new(font_family);
-    //doc.set_title("Demo doc");
+    let font_family = genpdf::fonts::from_files("./fonts", "Roboto", None)
+            .expect("Failed to load font family");
+    let mut doc = genpdf::Document::new(font_family);
+    doc.set_title("Demo doc");
 
     //// Customize page
-    //let mut decorator = genpdf::SimplePageDecorator::new();
-    //decorator.set_margins(50);
+    let mut decorator = genpdf::SimplePageDecorator::new();
+    decorator.set_margins(50);
 
-    //doc.set_page_decorator(decorator);
+    doc.set_page_decorator(decorator);
+
+
+    let json:SpekterDocument = read_json();
+    doc = parse_blocks(doc, json);
+
+    // Create
 
     //let mut table = elements::TableLayout::new(vec![1,1]);
     //table.set_cell_decorator(elements::FrameCellDecorator::new(false, false, false));
@@ -100,8 +120,8 @@ fn main() {
 
     //doc.push(table);
 
-    //doc.render_to_file("output.pdf").expect("Failed to write PDF file");
+    doc.render_to_file("output.pdf").expect("Failed to write PDF file");
 
-    println!("Hello, world!");
+    println!("File rendered to output.pdf");
 
 }
